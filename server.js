1546d09d4926368eea5e5f06641f38871a8365b5
Santhosh Kumar J <santhujayanna459@gmail.com>
2025-10-30 19:12:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,4 +46,4 @@ app.post('/submit', (req, res) => {
 });
 
 // Start server
-app.listen(port, () => console.log(`🚀 Server running on http://localhost:${port}`));
+app.listen(port, () => console.log(`🚀  Server running on http://localhost:${port}`));
